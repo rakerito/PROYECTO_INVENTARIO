335bca00414db0bfd79a5f3470d285939530379a
Raquel <raquel.pastor.gaytan@gmail.com>
2026-03-20 12:15:09 -0600

    Agregar productos

--- app/main.py
+++ app/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Datos de prueba (Simulando una base de datos)
+
 products = [
     {"id": "1", "name": "Laptop", "price": 1200.0},
-    {"id": "2", "name": "Mouse", "price": 25.0}
+    {"id": "2", "name": "Mouse", "price": 25.0},
+    {"id": "3", "name": "Teclado", "price": 50.0},
+    {"id": "4", "name": "Monitor", "price": 300.0},
+    {"id": "5", "name": "USB", "price": 10.0},
+    {"id": "6", "name": "Audífonos", "price": 80.0},
+    {"id": "7", "name": "Webcam", "price": 60.0},
+    {"id": "8", "name": "Impresora", "price": 200.0},
+    {"id": "9", "name": "Tablet", "price": 250.0},
+    {"id": "10", "name": "Celular", "price": 500.0},
+    {"id": "11", "name": "Cargador", "price": 20.0},
+    {"id": "12", "name": "Bocina", "price": 90.0},
 ]
 
 @app.get("/products")
